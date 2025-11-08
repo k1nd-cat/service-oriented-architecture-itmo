c23890ec065820840276841e3988daa73c1475e4
michael-bill <biloshitskiy_m@mail.ru>
2025-11-08 10:53:07 +0000

    fixes

--- movie-service/src/main/java/ru/itmo/soa/movie/mapper/MovieMappingCustomizer.java
+++ movie-service/src/main/java/ru/itmo/soa/movie/mapper/MovieMappingCustomizer.java
@@ -2,14 +2,21 @@ package ru.itmo.soa.movie.mapper;
 
 import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.stereotype.Component;
 import ru.itmo.soa.movie.entity.MovieEntity;
+import ru.itmo.soa.movie.entity.PersonEntity;
 
 @Component
 public class MovieMappingCustomizer implements ModelMapperCustomizer {
 
     @Override
     public void apply(ModelMapper modelMapper) {
+        // Используем STRICT matching strategy, чтобы избежать неправильного маппинга полей
+        modelMapper.getConfiguration()
+            .setMatchingStrategy(MatchingStrategies.STRICT)
+            .setAmbiguityIgnored(true);
+        
         // Маппинг из MovieEntity в Movie (Public API) с конвертацией Long id -> Integer id
         modelMapper.createTypeMap(MovieEntity.class, ru.itmo.soa.movie.dto.publicapi.Movie.class)
                 .addMappings(mapper -> {
@@ -31,6 +38,18 @@ public class MovieMappingCustomizer implements ModelMapperCustomizer {
                 .addMappings(mapper -> {
                     mapper.skip(MovieEntity::setId);
                     mapper.skip(MovieEntity::setCreationDate);
+                });
+
+        // Маппинг из Person (Public API DTO) в PersonEntity - игнорируем id
+        modelMapper.createTypeMap(ru.itmo.soa.movie.dto.publicapi.Person.class, PersonEntity.class)
+                .addMappings(mapper -> {
+                    mapper.skip(PersonEntity::setId);
+                });
+
+        // Маппинг из Person (Internal API DTO) в PersonEntity - игнорируем id
+        modelMapper.createTypeMap(ru.itmo.soa.movie.dto.internal.Person.class, PersonEntity.class)
+                .addMappings(mapper -> {
+                    mapper.skip(PersonEntity::setId);
                 });
     }
 }
