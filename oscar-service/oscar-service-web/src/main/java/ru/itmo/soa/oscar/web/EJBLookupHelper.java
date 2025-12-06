d1df05e20fca2a21e050457da046e53db4cc8be8
michael-bill <biloshitskiy_m@mail.ru>
2025-12-06 14:47:38 +0000

    ejb fix 2

--- oscar-service/oscar-service-web/src/main/java/ru/itmo/soa/oscar/web/EJBLookupHelper.java
+++ oscar-service/oscar-service-web/src/main/java/ru/itmo/soa/oscar/web/EJBLookupHelper.java
@@ -5,31 +5,35 @@ import ru.itmo.soa.oscar.service.OscarService;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.Properties;
 
 public class EJBLookupHelper {
 
-    private static final Logger log = Logger.getLogger(EJBLookupHelper.class.getName());
+    private static final String REMOTE_HOST = "localhost";
+    private static final String IIOP_PORT = "3700";
 
-    private static final String OSCAR_SERVICE_JNDI = 
-        "java:global/oscar-service/ru.itmo.soa-oscar-service-ejb-1.0.0/OscarServiceBean!ru.itmo.soa.oscar.service.OscarService";
+    private static final String JNDI_NAME = "java:global/oscar-service/ru.itmo.soa-oscar-service-ejb-1.0.0/OscarServiceBean";
 
     public static OscarService lookupRemoteOscarService() {
         Context ctx = null;
         try {
-            ctx = new InitialContext();
-            OscarService service = (OscarService) ctx.lookup(OSCAR_SERVICE_JNDI);
+            Properties props = new Properties();
+            props.setProperty(Context.INITIAL_CONTEXT_FACTORY, 
+                "com.sun.enterprise.naming.SerialInitContextFactory");
+            props.setProperty("org.omg.CORBA.ORBInitialHost", REMOTE_HOST);
+            props.setProperty("org.omg.CORBA.ORBInitialPort", IIOP_PORT);
+
+            ctx = new InitialContext(props);
+            OscarService service = (OscarService) ctx.lookup(JNDI_NAME);
+
             return service;
         } catch (NamingException e) {
-            throw new RuntimeException("Failed to lookup remote EJB", e);
+            throw new RuntimeException("Failed to lookup remote EJB via IIOP: " + e.getMessage(), e);
         } finally {
             if (ctx != null) {
                 try {
                     ctx.close();
-                } catch (NamingException e) {
-                    log.log(Level.WARNING, "Failed to close context", e);
-                }
+                } catch (NamingException e) { }
             }
         }
     }
